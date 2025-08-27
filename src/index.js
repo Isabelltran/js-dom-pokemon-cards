d747aa908e901475d4e3e47b81dd3977bce58845
Isabell <isabell.tran@no.experis.com>
2025-08-27 14:23:18 +0200

    Core Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 
-console.log(data);
+function renderTasks(){
+    console.log("In renderTasks")
+    console.log(data)
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+    for (let i = 0; i < data.length; i++){
+        const card = data[i]
+        const cardLi = document.createElement('li')
+
+        card.className = 'card';
+        const title = document.createElement('h2');
+        title.className = 'card--title';
+        title.textContent = data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1).toLowerCase();
+
+        const image = document.createElement('img');
+        image.className = 'card--img';
+        image.width = 256;
+        image.src = data[i].sprites.other['official-artwork'].front_default;
+
+        const stats = document.createElement('ul');
+        stats.className = 'card--text';
+
+        data[i].stats.forEach(stat => {
+        const statItem = document.createElement('li');
+        statItem.textContent = `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`;
+        stats.appendChild(statItem);
+        });
+        
+        cardLi.className = 'card';
+        cardLi.appendChild(title);
+        cardLi.appendChild(image);
+        cardLi.appendChild(stats);
+        document.querySelector('.cards').appendChild(cardLi);
+    }
+}
+
+function main(){
+    renderTasks()
+
+}
+main()
+
